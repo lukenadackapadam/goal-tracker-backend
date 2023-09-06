06df2bffadcbbac9ed7242985d68a11b7d7ead81
luke <luke.nadackapadam@gmail.com>
2023-09-05 23:23:00 -0500

    Add index & create actions

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,19 +1,22 @@
 class GoalsController < ApplicationController
+  before_action :authenticate_user
+
   def index
-    goals = Goal.all
+    @goals = current_user.goals
+    render template: "goals/index"
   end
 
   def create
     goal = Goal.new(
-      user_id: params[:user_id],
+      user_id: current_user.id,
       description: params[:description],
-      type: params[:type],
+      goal_type: params[:goal_type],
       completed: params[:completed],
     )
-    if user.save
-      render json: { message: "Goal created successfully" }, status: :created
+    if goal.save
+      render json: { message: "Goal created successfully!" }, status: :created
     else
-      render json: { errors: user.errors.full_messages }, status: :bad_request
+      render json: { message: "Please login first!" }, status: :bad_request
     end
   end
 end
---
+++ app/views/goals/_goal.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! goal, :id, :user_id, :description, :goal_type, :completed, :created_at, :updated_at
