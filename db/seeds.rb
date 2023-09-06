c52304d7968f39db205e83bde91e829880649464
luke <luke.nadackapadam@gmail.com>
2023-09-06 11:40:46 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!([
+  { first_name: "Squidward", last_name: "Ward", password_digest: "password", email: "squid@example.com" },
+  { first_name: "Spongebob", last_name: "Square", password_digest: "password", email: "sponge@example.com" },
+  { first_name: "Patrick", last_name: "Star", password_digest: "password", email: "pat@example.com" },
+])
+
+Goal.create!([
+  { user_id: 2, description: "Work work work!", goal_type: "Daily", completed: false },
+  { user_id: 2, description: "Play with Patrick!", goal_type: "Weekly", completed: false },
+  { user_id: 1, description: "Practice clarinet.", goal_type: "Daily", completed: true },
+  { user_id: 3, description: "Play with Spongebob!", goal_type: "Daily", completed: true },
+])
