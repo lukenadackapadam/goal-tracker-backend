8a44f573b95aa206bbe6056454ac14a185634344
luke <luke.nadackapadam@gmail.com>
2023-09-06 11:47:21 -0500

    Cleanup seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 User.create!([
   { first_name: "Squidward", last_name: "Ward", password_digest: "password", email: "squid@example.com" },
   { first_name: "Spongebob", last_name: "Square", password_digest: "password", email: "sponge@example.com" },
