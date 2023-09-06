0dd6a6406ddaca78219c453a879b6a86686a1925
luke <luke.nadackapadam@gmail.com>
2023-09-05 23:46:23 -0500

    Add update action for goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -7,16 +7,31 @@ class GoalsController < ApplicationController
   end
 
   def create
-    goal = Goal.new(
+    @goal = Goal.new(
       user_id: current_user.id,
       description: params[:description],
       goal_type: params[:goal_type],
       completed: params[:completed],
     )
-    if goal.save
+    if @goal.save
       render json: { message: "Goal created successfully!" }, status: :created
     else
       render json: { message: "Please login first!" }, status: :bad_request
+    end
+  end
+
+  def update
+    @goal = Goal.find_by(id: params[:id])
+    @goal.update(
+      user_id: params[:user_id] || @goal.user_id,
+      description: params[:description] || @goal.description,
+      goal_type: params[:goal_type] || @goal.goal_type,
+      completed: params[:completed] || @goal.completed,
+    )
+    if @goal.save
+      render json: { message: "Goal updated successfully!" }, status: :created
+    else
+      render json: { errors: @goal.errors.full_messages }, status: :unprocessable_entity
     end
   end
 end
