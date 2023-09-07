ee4c53d1e3c0624f837b5e27aacc40aebcbe26fa
luke <luke.nadackapadam@gmail.com>
2023-09-07 11:18:43 -0500

    Comment out seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-User.create!([
-  { first_name: "Squidward", last_name: "Ward", password_digest: "password", email: "squid@example.com" },
-  { first_name: "Spongebob", last_name: "Square", password_digest: "password", email: "sponge@example.com" },
-  { first_name: "Patrick", last_name: "Star", password_digest: "password", email: "pat@example.com" },
-])
+# User.create!([
+#   { first_name: "Squidward", last_name: "Ward", password_digest: "password", email: "squid@example.com" },
+#   { first_name: "Spongebob", last_name: "Square", password_digest: "password", email: "sponge@example.com" },
+#   { first_name: "Patrick", last_name: "Star", password_digest: "password", email: "pat@example.com" },
+# ])
 
-Goal.create!([
-  { user_id: 2, description: "Work work work!", goal_type: "Daily", completed: false },
-  { user_id: 2, description: "Play with Patrick!", goal_type: "Weekly", completed: false },
-  { user_id: 1, description: "Practice clarinet.", goal_type: "Daily", completed: true },
-  { user_id: 3, description: "Play with Spongebob!", goal_type: "Daily", completed: true },
-])
+# Goal.create!([
+#   { user_id: 2, description: "Work work work!", goal_type: "Daily", completed: false },
+#   { user_id: 2, description: "Play with Patrick!", goal_type: "Weekly", completed: false },
+#   { user_id: 1, description: "Practice clarinet.", goal_type: "Daily", completed: true },
+#   { user_id: 3, description: "Play with Spongebob!", goal_type: "Daily", completed: true },
+# ])
